a9b9161d91f8586859da4715520aff98496c8d56
PasakornPh <59168511+PasakornPh@users.noreply.github.com>
2021-06-03 20:24:25 +0700

    Add example simple test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -30,4 +30,56 @@ describe('AppController', () => {
 
   });
 
+  // =====================================================================================================
+
+  // Example Simple Test  ref. https://jestjs.io/docs/using-matchers
+
+  beforeAll(() => {
+    // call before all test
+  })
+
+  afterAll(() => {
+    // call after all test
+  })
+
+  beforeEach(() => {
+    // call every before test case
+  })
+
+  afterEach(() => {
+    // call every after test case
+  })
+
+  test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);   // toBeGreaterThan, toBeGreaterThanOrEqual, toBeLessThan, toBeLessThanOrEqual
+  });
+
+  test('adding positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
+        expect(a + b).not.toBe(0);
+      }
+    }
+  });
+
+  test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+  });
+
+  test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+  });
+
+  const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'beer',
+  ];
+  
+  test('the shopping list has beer on it', () => {
+    expect(shoppingList).toContain('beer');
+  });
+
 });
